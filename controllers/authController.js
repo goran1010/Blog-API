75f747af7c11ada1849454975336b5b1b9d0888b
Goran Jovic <goran1010jovic@gmail.com>
2025-07-14 18:54:41 +0200

    Fix env

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import * as usersModel from "../models/usersModel.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const MY_SECRET_KEY = process.env.MY_SECRET_KEY;
 
 export async function logIn(req, res) {
   try {
@@ -12,19 +15,17 @@ export async function logIn(req, res) {
     const match = await bcrypt.compare(password, user.password);
 
     if (match) {
-      const token = jwt.sign({ id: user.id }, "mySecretKey", {
+      const token = jwt.sign({ id: user.id }, MY_SECRET_KEY, {
         expiresIn: "30d",
       });
-      res
-        .status(200)
-        .json({
-          user: {
-            token,
-            id: user.id,
-            username: user.username,
-            isAuthor: user.isAuthor,
-          },
-        });
+      res.status(200).json({
+        user: {
+          token,
+          id: user.id,
+          username: user.username,
+          isAuthor: user.isAuthor,
+        },
+      });
     } else {
       res.status(401).json({ message: "Invalid credentials" });
     }
